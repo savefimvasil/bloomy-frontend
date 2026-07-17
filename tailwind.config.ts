dacdc80feb86349c4bd1f921597ab2e7736cf5cc
y.savchyn <savefimvasil@gmail.com>
2026-07-17 22:03:27 +0100

    fix build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../bloomy-packages/planner/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
